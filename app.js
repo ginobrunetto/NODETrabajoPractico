a4ae47abb033536da492f1a5bfb495af34cb9569
Gino Brunetto <52689333+ginobrunetto@users.noreply.github.com>
2020-05-19 20:47:09 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-//1-Create a method to get all the files inside given path.
-//2-create a method to print all the files.
-//3-Create a method to inverse sort and print the files of a given path
-//4-create a method to get the amount of files starting with one letter.
-// POSTDATA:  AL EJECUTAR ESTA FUNCIÓN SE CUMPLEN LAS 4 CONSIGNAS.
-//            TUVE LA INTENCIÓN DE PONER UN SEGUNDO STDIN PARA INGRESAR LA LETRA MANUALMENTE 
-//            DESDE CONSOLA PERO NO LOGRÉ IMPLEMENTAR AMBOS, SE ME SUPERPONIAN
+const readline = require("readline");
 const fs = require('fs');
 
-( function() {
-        console.log("Escribe tu path:");
-        var filesGivenPath=[];
-        var letter = 'a';
-        var contador = 0;
-        var stdin = process.openStdin();        
-        stdin.addListener("data", function(d) {
-            filesGivenPath = fs.readdirSync(d.toString().trim());
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-            console.log("Tus files son: " , filesGivenPath);
-            console.log('Tus files ordenados a la inversa son: ', filesGivenPath.reverse());
+var filesGivenPath=[];
+var letter
+var contador = 0;
 
-            filesGivenPath.forEach(Element => {
-                if (Element.startsWith(letter)) {
-                    contador++;
-                }
-            });
-    
-            console.log('La cantidad de files que comienzan con la letra: "', letter, '" es: ', contador);
+rl.question("Escribe tu path:", function(aa) {
+    filesGivenPath = fs.readdirSync(aa);
+    rl.question("Elige una letra:", function(bb) {
+        letter = bb;
+         rl.close();
+     });    
+});
 
-            process.exit();
-        });        
-}
-)();
+rl.on("close", function() {
+    filesGivenPath.forEach(Element => {
+        if (Element.startsWith(letter)) {
+            contador++;
+        }
+    });
+    console.log("Tus files son: " , filesGivenPath);
+    console.log('Tus files ordenados a la inversa son: ', filesGivenPath.reverse());
+    console.log('La cantidad de files que comienzan con la letra: "', letter, '" es: ', contador);    
+    process.exit(0);
+});
